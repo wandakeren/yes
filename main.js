46020c2b25665ad450f7928a8682ce4615985acf
wanda prasetya <109602063+wandakeren@users.noreply.github.com>
2022-07-28 17:06:47 +0700

    Update main.js

--- main.js
+++ main.js
@@ -131,7 +131,7 @@ async function connectionUpdate(update) {
   }
   if (global.db.data == null) await loadDatabase()
   console.log(JSON.stringify(update, null, 4))
-  if (update.receivedPendingNotifications) conn.sendMessage(`6287898307350@s.whatsapp.net`, {text: 'BERHASIL TERSAMBUNG KE BOT ✿𝐆𝐞𝐦𝐩𝐲 𝐓𝐨𝐧✿ JANGAN LUPA SAVE GEMPY, PASTI DI SAVEBACK' })
+  if (update.receivedPendingNotifications) conn.sendMessage(`628975217200@s.whatsapp.net`, {text: 'BERHASIL TERSAMBUNG KE BOT ✿𝐒𝐀𝐍𝐒 𝐁𝐎𝐓✿ JANGAN LUPA SAVE 𝐒𝐀𝐍𝐒 𝐁𝐎𝐓, PASTI DI SAVEBACK' })
 }
 
 
@@ -167,8 +167,8 @@ global.reloadHandler = function (restatConn) {
     conn.ev.off('creds.update', conn.credsUpdate)
   }
 
-  conn.welcome = 'HALO @user 👋\n SELAMAT DATANG DI  @subject \n❋𝗝𝗔𝗦𝗔 𝗥𝗨𝗡❋\n-𝚃𝚒𝚗𝚐𝚐𝚊𝚕 𝚂𝚌𝚊𝚗\n-𝟸𝟺𝙹𝚊𝚖 𝚋𝚘𝚝 𝚊𝚔𝚝𝚒𝚏\n𝙿𝚊𝚢𝚖𝚎𝚗𝚝 : 𝙳𝙰𝙽𝙰\n𝙼𝚒𝚗𝚊𝚝 ? wa.me/6287898307350' 
-  conn.bye = 'YAHHH @user TELAH KELUAR DARI @subject'
+  conn.welcome = 'HALO @user 👋\n SELAMAT DATANG DI Group @subject \n❋𝗝𝗔𝗦𝗔 𝗥𝗨𝗡❋\n-𝚃𝚒𝚗𝚐𝚐𝚊𝚕 𝚂𝚌𝚊𝚗\n-𝟸𝟺𝙹𝚊𝚖 𝚋𝚘𝚝 𝚊𝚔𝚝𝚒𝚏\n𝙿𝚊𝚢𝚖𝚎𝚗𝚝 : 𝙳𝙰𝙽𝙰\n𝙼𝚒𝚗𝚊𝚝 ? wa.me/628975217200' 
+  conn.bye = 'YAHHH @user TELAH KELUAR DARI Group @subject'
   conn.spromote = '@user sekarang admin!'
   conn.sdemote = '@user sekarang bukan admin!'
   conn.handler = handler.handler.bind(conn)
