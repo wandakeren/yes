dcf4f72769e81e134bb63796360ada4e7d5e6224
wanda prasetya <109602063+wandakeren@users.noreply.github.com>
2022-07-28 17:26:10 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,10 @@
 let handler = async m => m.reply(`
-            .✵.𝗚𝗥𝗨𝗣 𝗣𝗘𝗡𝗧𝗔𝗕𝗢𝗧.✵.
-
-            https://chat.whatsapp.com/ElBB5NrJS637TYqjJLHuk2
+            .✵.𝐆𝐑𝐎𝐔𝐏 𝐌𝐄𝐌𝐄.✵.
+  https://chat.whatsapp.com/J6WuCY7cO6RKCdgTK11JWw
+  𝐁𝐲𝐞 𝐖𝐚𝐧𝐝𝐚 𝐆𝐚𝐧𝐬
 `.trim()) // Tambah sendiri kalo mau
-handler.help = ['donasi']
-handler.tags = ['info']
+handler.help = ['08975217200 (pulsa)']
+handler.tags = ['Owner Gw Jomblo Bang Kali Aj Ada Yang Mau']
 handler.command = /^dona(te|si)$/i
 
 module.exports = handler
