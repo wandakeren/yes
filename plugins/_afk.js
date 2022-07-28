70b2e6409a02a61538efef9965fa530a8b5571bb
wanda prasetya <109602063+wandakeren@users.noreply.github.com>
2022-07-28 17:32:54 +0700

    Update _afk.js

--- plugins/_afk.js
+++ plugins/_afk.js
@@ -17,7 +17,7 @@ Selama ${clockString(new Date - user.afk)}
       if (!afkTime || afkTime < 0) continue
       let reason = user.afkReason || ''
       m.reply(`
-Jangan tag dia!
+Jangan tag dia Njink!
 Dia sedang AFK ${reason ? 'dengan alasan ' + reason : 'tanpa alasan'}
 Selama ${clockString(new Date - afkTime)}
 `.trim())
